bac66ee3d7842b43ae8d5e569d0aa8d41f12ded9
Xyntexx <21282105+Xyntexx@users.noreply.github.com>
2025-11-02 13:35:08 +0200

    Add CLI interface for batch component processing
    - Created cli.py for batch processing components from JSON
    - Implemented grid-based symbol positioning (5 columns)
    - Added position parameters to symbol generation functions
    - Updated symbols to use input reference designators
    - Split comma-separated references into individual symbols
    - Calculate absolute coordinates for text properties
    - Remove hardcoded UUIDs (KiCad auto-generates)
    - add gitignore

--- create_symbol.py
+++ create_symbol.py
@@ -16,11 +16,16 @@ packages_C = {"0402": "C_0402_1005Metric",
               "2512": "C_2512_6332Metric"}
 
 
-def createResistorSymbol(value, package, lcsc):
+def createResistorSymbol(value, package, lcsc, x=0, y=0, ref="R0"):
     package_lib = packages_R[package]
     value = convertToShorthandNotation(value)
+    # Calculate absolute positions for properties
+    ref_x = x + 2.54
+    ref_y = y - 1.2701
+    val_x = x + 2.54
+    val_y = y + 1.2699
     s = f"""(lib_symbols
-  (symbol "Device:R" (pin_numbers hide) (pin_names (offset 0)) (in_bom yes) (on_board yes)
+  (symbol "Device:R" (pin_numbers hide) (pin_names (offset 0)) (exclude_from_sim no) (in_bom yes) (on_board yes)
     (property "Reference" "R" (at 2.032 0 90)
       (effects (font (size 1.27 1.27)))
     )
@@ -28,19 +33,19 @@ def createResistorSymbol(value, package, lcsc):
       (effects (font (size 1.27 1.27)))
     )
     (property "Footprint" "" (at -1.778 0 90)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (property "Datasheet" "~" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "Description" "Resistor" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (property "ki_keywords" "R res resistor" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
-    )
-    (property "ki_description" "Resistor" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (property "ki_fp_filters" "R_*" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (symbol "R_0_1"
       (rectangle (start -1.016 -2.54) (end 1.016 2.54)
@@ -61,38 +66,236 @@ def createResistorSymbol(value, package, lcsc):
   )
 )
 
-(symbol (lib_id "Device:R") (at 133.35 93.98 270) (unit 1)
-  (in_bom yes) (on_board yes) (dnp no)
-  (uuid 3bdd9ea2-b74c-4d3b-9499-90745dafde71)
-  (property "Reference" "R101" (at 133.35 96.52 90)
-    (effects (font (size 1.27 1.27)))
+(symbol (lib_id "Device:R") (at {x} {y} 180) (unit 1)
+  (exclude_from_sim no) (in_bom yes) (on_board yes) (dnp no) (fields_autoplaced yes)
+  (property "Reference" "{ref}" (at {ref_x} {ref_y} 0)
+    (effects (font (size 1.27 1.27)) (justify right))
   )
-  (property "Value" "{value}" (at 134.62 91.44 90)
-    (effects (font (size 1.27 1.27)))
+  (property "Value" "{value}" (at {val_x} {val_y} 0)
+    (effects (font (size 1.27 1.27)) (justify right))
   )
-  (property "Footprint" "Resistor_SMD:{package_lib}" (at 133.35 92.202 90)
-    (effects (font (size 1.27 1.27)) hide)
+  (property "Footprint" "Resistor_SMD:{package_lib}" (at 1.778 0 90)
+    (effects (font (size 1.27 1.27)) (hide yes))
   )
-  (property "Datasheet" "~" (at 133.35 93.98 0)
-    (effects (font (size 1.27 1.27)) hide)
+  (property "Datasheet" "~" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
   )
-  (property "LCSC" "C{lcsc}" (at 133.35 93.98 0)
-    (effects (font (size 1.27 1.27)) hide)
+  (property "Description" "Resistor" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
   )
-  (pin "1" (uuid 6e207a43-f9f8-4882-b011-013523d3670d))
-  (pin "2" (uuid 5d2b3056-f435-42cd-b7f4-1678fa653eac))
+  (property "LCSC" "C{lcsc}" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (pin "2" (uuid "")
+  )
+  (pin "1" (uuid "")
+  )
   (instances
+    (project ""
+      (path ""
+        (reference "{ref}") (unit 1)
+      )
+    )
   )
 )
 """
-    return s.format(value=value, package_lib=package_lib, lcsc=lcsc)
+    return s.format(value=value, package_lib=package_lib, lcsc=lcsc, x=x, y=y, ref=ref, ref_x=ref_x, ref_y=ref_y, val_x=val_x, val_y=val_y)
 
 
-def createCapacitorSymbol(value, package, lcsc, voltage):
+def createResistorSymbolSmall(value, package, lcsc, x=0, y=0, ref="R0"):
+    package_lib = packages_R[package]
+    value = convertToShorthandNotation(value)
+    # Calculate absolute positions for properties
+    ref_x = x + 2.54
+    ref_y = y - 1.2701
+    val_x = x + 2.54
+    val_y = y + 1.2699
+    s = f"""(lib_symbols
+  (symbol "Device:R_Small" (pin_numbers hide) (pin_names (offset 0.254) hide) (exclude_from_sim no) (in_bom yes) (on_board yes)
+    (property "Reference" "R" (at 0.762 0.508 0)
+      (effects (font (size 1.27 1.27)) (justify left))
+    )
+    (property "Value" "R_Small" (at 0.762 -1.016 0)
+      (effects (font (size 1.27 1.27)) (justify left))
+    )
+    (property "Footprint" "" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "Datasheet" "~" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "Description" "Resistor, small symbol" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "ki_keywords" "R resistor" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "ki_fp_filters" "R_*" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (symbol "R_Small_0_1"
+      (rectangle (start -0.762 1.778) (end 0.762 -1.778)
+        (stroke (width 0.2032) (type default))
+        (fill (type none))
+      )
+    )
+    (symbol "R_Small_1_1"
+      (pin passive line (at 0 2.54 270) (length 0.762)
+        (name "~" (effects (font (size 1.27 1.27))))
+        (number "1" (effects (font (size 1.27 1.27))))
+      )
+      (pin passive line (at 0 -2.54 90) (length 0.762)
+        (name "~" (effects (font (size 1.27 1.27))))
+        (number "2" (effects (font (size 1.27 1.27))))
+      )
+    )
+  )
+)
+
+(symbol (lib_id "Device:R_Small") (at {x} {y} 0) (unit 1)
+  (exclude_from_sim no) (in_bom yes) (on_board yes) (dnp no) (fields_autoplaced yes)
+  (property "Reference" "{ref}" (at {ref_x} {ref_y} 0)
+    (effects (font (size 1.27 1.27)) (justify left))
+  )
+  (property "Value" "{value}" (at {val_x} {val_y} 0)
+    (effects (font (size 1.27 1.27)) (justify left))
+  )
+  (property "Footprint" "Resistor_SMD:{package_lib}" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (property "Datasheet" "~" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (property "Description" "Resistor, small symbol" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (property "LCSC" "C{lcsc}" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (pin "2" (uuid "")
+  )
+  (pin "1" (uuid "")
+  )
+  (instances
+    (project ""
+      (path ""
+        (reference "{ref}") (unit 1)
+      )
+    )
+  )
+)
+"""
+    return s.format(value=value, package_lib=package_lib, lcsc=lcsc, x=x, y=y, ref=ref, ref_x=ref_x, ref_y=ref_y, val_x=val_x, val_y=val_y)
+
+
+def createCapacitorSymbol(value, package, lcsc, voltage, x=0, y=0, ref="C0"):
     package_lib = packages_C[package]
     value = convertToShorthandNotation(value)
+    # Calculate absolute positions for properties
+    ref_x = x + 3.81
+    ref_y = y - 1.2701
+    val_x = x + 3.81
+    val_y = y + 1.2699
     s = f"""(lib_symbols
-  (symbol "Device:C_Small" (pin_numbers hide) (pin_names (offset 0.254) hide) (in_bom yes) (on_board yes)
+  (symbol "Device:C" (pin_numbers hide) (pin_names (offset 0.254)) (exclude_from_sim no) (in_bom yes) (on_board yes)
+    (property "Reference" "C" (at 0.635 2.54 0)
+      (effects (font (size 1.27 1.27)) (justify left))
+    )
+    (property "Value" "C" (at 0.635 -2.54 0)
+      (effects (font (size 1.27 1.27)) (justify left))
+    )
+    (property "Footprint" "" (at 0.9652 -3.81 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "Datasheet" "~" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "Description" "Unpolarized capacitor" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "ki_keywords" "cap capacitor" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "ki_fp_filters" "C_*" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (symbol "C_0_1"
+      (polyline
+        (pts
+          (xy -2.032 -0.762)
+          (xy 2.032 -0.762)
+        )
+        (stroke (width 0.508) (type default))
+        (fill (type none))
+      )
+      (polyline
+        (pts
+          (xy -2.032 0.762)
+          (xy 2.032 0.762)
+        )
+        (stroke (width 0.508) (type default))
+        (fill (type none))
+      )
+    )
+    (symbol "C_1_1"
+      (pin passive line (at 0 3.81 270) (length 2.794)
+        (name "~" (effects (font (size 1.27 1.27))))
+        (number "1" (effects (font (size 1.27 1.27))))
+      )
+      (pin passive line (at 0 -3.81 90) (length 2.794)
+        (name "~" (effects (font (size 1.27 1.27))))
+        (number "2" (effects (font (size 1.27 1.27))))
+      )
+    )
+  )
+)
+
+(symbol (lib_id "Device:C") (at {x} {y} 0) (unit 1)
+  (exclude_from_sim no) (in_bom yes) (on_board yes) (dnp no) (fields_autoplaced yes)
+  (property "Reference" "{ref}" (at {ref_x} {ref_y} 0)
+    (effects (font (size 1.27 1.27)) (justify left))
+  )
+  (property "Value" "{value} {voltage}" (at {val_x} {val_y} 0)
+    (effects (font (size 1.27 1.27)) (justify left))
+  )
+  (property "Footprint" "Capacitor_SMD:{package_lib}" (at 0.9652 3.81 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (property "Datasheet" "~" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (property "Description" "Unpolarized capacitor" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (property "LCSC" "C{lcsc}" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (pin "1" (uuid "")
+  )
+  (pin "2" (uuid "")
+  )
+  (instances
+    (project ""
+      (path ""
+        (reference "{ref}") (unit 1)
+      )
+    )
+  )
+)
+"""
+    return s.format(value=value, package_lib=package_lib, lcsc=lcsc, voltage=voltage, x=x, y=y, ref=ref, ref_x=ref_x, ref_y=ref_y, val_x=val_x, val_y=val_y)
+
+
+def createCapacitorSymbolSmall(value, package, lcsc, voltage, x=0, y=0, ref="C0"):
+    package_lib = packages_C[package]
+    value = convertToShorthandNotation(value)
+    # Calculate absolute positions for properties
+    ref_x = x + 2.54
+    ref_y = y - 1.2638
+    val_x = x + 2.54
+    val_y = y + 1.2762
+    s = f"""(lib_symbols
+  (symbol "Device:C_Small" (pin_numbers hide) (pin_names (offset 0.254) hide) (exclude_from_sim no) (in_bom yes) (on_board yes)
     (property "Reference" "C" (at 0.254 1.778 0)
       (effects (font (size 1.27 1.27)) (justify left))
     )
@@ -100,19 +303,19 @@ def createCapacitorSymbol(value, package, lcsc, voltage):
       (effects (font (size 1.27 1.27)) (justify left))
     )
     (property "Footprint" "" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (property "Datasheet" "~" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
+    )
+    (property "Description" "Unpolarized capacitor, small symbol" (at 0 0 0)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (property "ki_keywords" "capacitor cap" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
-    )
-    (property "ki_description" "Unpolarized capacitor, small symbol" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (property "ki_fp_filters" "C_*" (at 0 0 0)
-      (effects (font (size 1.27 1.27)) hide)
+      (effects (font (size 1.27 1.27)) (hide yes))
     )
     (symbol "C_Small_0_1"
       (polyline
@@ -145,26 +348,37 @@ def createCapacitorSymbol(value, package, lcsc, voltage):
   )
 )
 
-(symbol (lib_id "Device:C_Small") (at 63.5 67.31 0) (mirror x) (unit 1)
-  (in_bom yes) (on_board yes) (dnp no)
-  (uuid af931f83-f0d7-4d1c-af01-4b5144b5078d)
-  (property "Reference" "C4" (at 66.04 68.58 0)
+(symbol (lib_id "Device:C_Small") (at {x} {y} 0) (unit 1)
+  (exclude_from_sim no) (in_bom yes) (on_board yes) (dnp no) (fields_autoplaced yes)
+  (property "Reference" "{ref}" (at {ref_x} {ref_y} 0)
     (effects (font (size 1.27 1.27)) (justify left))
   )
-  (property "Value" "{value} {voltage}" (at 66.04 66.04 0)
+  (property "Value" "{value} {voltage}" (at {val_x} {val_y} 0)
     (effects (font (size 1.27 1.27)) (justify left))
   )
-  (property "Footprint" "Capacitor_SMD:{package_lib}" (at 63.5 67.31 0)
-    (effects (font (size 1.27 1.27)) hide)
+  (property "Footprint" "Capacitor_SMD:{package_lib}" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
   )
-  (property "Datasheet" "~" (at 63.5 67.31 0)
-    (effects (font (size 1.27 1.27)) hide)
+  (property "Datasheet" "~" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
   )
-  (property "LCSC" "{lcsc}" (at 63.5 67.31 0)
-    (effects (font (size 1.27 1.27)) hide)
+  (property "Description" "Unpolarized capacitor, small symbol" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
   )
-  (pin "1" (uuid 2b58b1a6-f663-4b5d-955f-81a6a0feee1d))
-  (pin "2" (uuid 973279a1-bf9a-4b95-826e-cbf7c2d552a6))
+  (property "LCSC" "C{lcsc}" (at 0 0 0)
+    (effects (font (size 1.27 1.27)) (hide yes))
+  )
+  (pin "1" (uuid "")
+  )
+  (pin "2" (uuid "")
+  )
+  (instances
+    (project ""
+      (path ""
+        (reference "{ref}") (unit 1)
+      )
+    )
+  )
 )
 """
-    return s.format(value=value, package_lib=package_lib, lcsc=lcsc, voltage=voltage)
+    return s.format(value=value, package_lib=package_lib, lcsc=lcsc, voltage=voltage, x=x, y=y, ref=ref, ref_x=ref_x, ref_y=ref_y, val_x=val_x, val_y=val_y)
